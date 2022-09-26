868f084c0aacf9eb8ef000d54c050d48f99f154b
dac <salih@cirgan.net>
2022-09-26 13:13:46 +0300

    kill em all

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta content="Wolfenstein" name="description"></meta>
+            <meta content="cirgan,wolfenstein,3d,game,salih cirgan" name="keywords"></meta>
+            <title>Wolfenstein 3D</title>
+            <link type="text/css" href="css/build.css" rel="stylesheet"></link>
+			<script src="js/webkitAudioContextMonkeyPatch.js" type="text/javascript" language="javascript"></script>
+            <script async="" src="js/a.js"></script>
+            <script language="javascript" type="text/javascript" src="js/build.js"></script>
+            <link href="images/favicon.png" type="image/png" rel="shortcut icon"></link>
+            <link href="images/favicon.png" type="image/png" rel="icon"></link>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-1695425028575499",
+          enable_page_level_ads: true
+     });
+</script>
+        </head>
+        <body bgcolor="#0000" oncontextmenu="return false;">
+            <div id="resources">
+Loading...<br></br>
+<img src="images/loading-intersect.png"/>
+            </div>
+            <audio id="music" preload="true" loop=""></audio>
+            <div>
+                <div id="copyright" class=" fadein">
+                    <div class="content" style="position: fixed; left: 0; top: 0; bottom: 0; right: 0; overflow: auto; padding: 32px;">
+                        <div style="width: 100%; text-align: center;">
+                            <img src="images/wolfenstein3d-logo.png"></img>
+                        </div>
+                        <div class="clear"></div>
+                        <div id="start" class="text readme" style="cursor: pointer; Color:black;">
+<img src="images/start.png"/>                       </div>
+
+
+
+                    </div>
+                </div>
+
+        </body>
+    </html>
